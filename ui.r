6cee7f0868ec139190a59dac420548c919a1d398
jedirafa <rafaelm.godinho@gmail.com>
2017-09-12 22:50:05 -0300

    #1

---
+++ ui.r
@@ -0,0 +1,65 @@
+#---
+#title: "Data Science Capstone"
+#author: "Rafael Godinho"
+#date: "september, 12 of 2017"
+#---
+
+
+suppressPackageStartupMessages(c(
+        library(shinythemes),
+        library(shiny),
+        library(tm),
+        library(stringr),
+        library(markdown),
+        library(stylo)))
+
+shinyUI(navbarPage("Data Science Capstone", 
+                   
+                   theme = shinytheme("sandstone"),
+                   
+ 
+                   ## Tab 1 - Prediction
+                   
+                   tabPanel("App",
+                            
+                            
+                            
+                            fluidRow(
+                                    
+                                    column(3),
+                                    column(6,
+                                           tags$div(textInput("text", 
+                                                              label = h3("Enter your text here:"),
+                                                              value = ),
+                                                    tags$span(style="color:grey",("Only English words are supported.")),
+                                                    br(),
+                                                    tags$hr(),
+                                                    h4("The predicted next word:"),
+                                                    tags$span(style="color:darkred",
+                                                              tags$strong(tags$h3(textOutput("predictedWord")))),
+                                                    br(),
+                                                    tags$hr(),
+                                                    h4("What you have entered:"),
+                                                    tags$em(tags$h4(textOutput("enteredWords"))),
+                                                    align="center")
+                                    ),
+                                    column(3)
+                            )
+                   ),
+                   
+
+                   ## Tab 2 - About 
+                   
+                   tabPanel("About",
+                            fluidRow(
+                                    column(2,
+                                           p("")),
+                                    column(8,
+                                           includeMarkdown("./about/about.md")),
+                                    column(2,
+                                           p(""))
+                            )
+                   )
+                  
+)
+)
